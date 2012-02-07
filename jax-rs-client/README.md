5890c0c8bf46cbcf19c0bc67562120d02ca95ec4
Blaine Mincey <blaine.mincey@gmail.com>
2012-02-06 19:48:48 -0500

    Corrected reference to helloworld-rs and added comments to src.  Corrected version in pom for JUnit and maven-compiler

--- jax-rs-client/README.md
+++ jax-rs-client/README.md
@@ -1,12 +1,14 @@
 External JAX-RS Client
 ======================
 
+
 Author: Blaine Mincey, blaine.mincey@redhat.com
 
 This example demonstrates the use an external JAX-RS RestEasy client
 which interacts with a JAX-RS Web service that uses *CDI 1.0* and *JAX-RS* 
 in *JBoss AS 7*.  Specifically, this client "calls" the HelloWorld JAX-RS
-Web Service created in quickstart #32.
+Web Service created in quickstart helloworld-rs.  Please refer to the helloworld-rs
+README.md for instructions on how to build and deploy helloworld-rs.
 
 Pre-requisite
 =============
@@ -22,8 +24,8 @@ Usage
 This example assumes that the following URLs are available based upon the 
 HelloWorld REST application being deployed:
 
-http://localhost:8080/rs-helloworld/xml
-http://localhost:8080/rs-helloworld/json
+http://localhost:8080/jboss-as-helloworld-rs/xml
+http://localhost:8080/jboss-as-helloworld-rs/json
 
 Execute the example by running 'mvn test'
 
@@ -32,7 +34,7 @@ requests to the Web Service.  Towards the end of the Maven build output, you
 should see the following for successful execution:
 
 ===============================================
-URL: http://localhost:8080/rs-helloworld/xml
+URL: http://localhost:8080/jboss-as-helloworld-rs/xml
 MediaType: application/xml
 
 *** Response from Server ***
@@ -41,7 +43,7 @@ MediaType: application/xml
 
 ===============================================
 ===============================================
-URL: http://localhost:8080/rs-helloworld/json
+URL: http://localhost:8080/jboss-as-helloworld-rs/json
 MediaType: application/json
 
 *** Response from Server ***
--- jax-rs-client/src/main/java/org/jboss/as/quickstarts/jaxrsClient/JaxRsClient.java
+++ jax-rs-client/src/main/java/org/jboss/as/quickstarts/jaxrsClient/JaxRsClient.java
@@ -13,17 +13,20 @@ import org.jboss.resteasy.client.ClientResponse;
 import javax.ws.rs.core.MediaType;
 
 /**
+ * The purpose of this class is to make an external REST call
+ * to the helloworld-rs RESTful application.
+ *
  * @author bmincey
  * 
  */
 public class JaxRsClient
 {
 
-    private static final String XML_URL = "http://localhost:8080/rs-helloworld/xml";
-    private static final String JSON_URL = "http://localhost:8080/rs-helloworld/json";
+    private static final String XML_URL = "http://localhost:8080/jboss-as-helloworld-rs/xml";
+    private static final String JSON_URL = "http://localhost:8080/jboss-as-helloworld-rs/json";
 
     /**
-     * 
+     *  Constructor initializing calls to runRequest method.
      */
     public JaxRsClient()
     {
@@ -32,9 +35,9 @@ public class JaxRsClient
     }
 
     /**
-     * 
-     * @param url
-     * @param mediaType
+     * The purpose of this method is to run the external REST request.
+     * @param url The url of the RESTful service
+     * @param mediaType The mediatype of the RESTful service
      */
     private void runRequest(String url, MediaType mediaType)
     {
@@ -44,20 +47,29 @@ public class JaxRsClient
         
         try
         {
+            // Using the RESTEasy libraries, initiate a client request 
+            // using the url as a parameter
             ClientRequest request = new ClientRequest(url);
+
+            // Be sure to set the mediatype of the request
             request.accept(mediaType);
 
+            // Request has been made, now let's get the response
             ClientResponse<String> response = request.get(String.class);
 
+            // Check the HTTP status of the request
+            // HTTP 200 indicates the request is OK
             if (response.getStatus() != 200)
             {
                 throw new RuntimeException("Failed request with HTTP status: "
                         + response.getStatus());
             }
 
+            // We have a good response, let's now read it
             BufferedReader br = new BufferedReader(new InputStreamReader(
                     new ByteArrayInputStream(response.getEntity().getBytes())));
 
+            // Loop over the br in order to print out the contents
             String output;
             System.out.println("\n*** Response from Server ***\n");
             while ((output = br.readLine()) != null)
@@ -83,6 +95,8 @@ public class JaxRsClient
     }
 
     /**
+     * This is the main method of the class which kicks off the processing
+     *
      * @param args
      */
     public static void main(String[] args)
