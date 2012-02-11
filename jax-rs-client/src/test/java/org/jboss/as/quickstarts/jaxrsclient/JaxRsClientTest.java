2ffb6f6a2feb9f487fe6f11198663b7d5e42ff2e
Blaine Mincey <blaine.mincey@gmail.com>
2012-02-11 16:04:23 -0500

    Moved original JaxRsClient class to test.  Using JUnit4 testing harness.

--- jax-rs-client/src/test/java/org/jboss/as/quickstarts/jaxrsclient/JaxRsClientTest.java
+++ jax-rs-client/src/test/java/org/jboss/as/quickstarts/jaxrsclient/JaxRsClientTest.java
@@ -1,46 +1,68 @@
+package org.jboss.as.quickstarts.jaxrsclient;
 /**
- * 
+ * This example demonstrates the use an external JAX-RS RestEasy client
+ * which interacts with a JAX-RS Web service that uses CDI 1.0 and JAX-RS 
+ * in JBoss AS 7.  Specifically, this client "calls" the HelloWorld JAX-RS
+ * Web Service created in quickstart helloworld-rs.  Please refer to the helloworld-rs
+ * README.md for instructions on how to build and deploy helloworld-rs.
  */
-package org.jboss.as.quickstarts.jaxrsClient;
+import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import javax.ws.rs.core.MediaType;
+
 import org.apache.http.client.ClientProtocolException;
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
-import javax.ws.rs.core.MediaType;
+import org.junit.Test;
+
 
 /**
- * The purpose of this class is to make an external REST call
- * to the helloworld-rs RESTful application.
- *
- * @author bmincey
+ * JUnit4 Test class which makes a request to the RESTful
+ * helloworld-rs web service.
  * 
+ * @author bmincey
+ *
  */
-public class JaxRsClient
+public class JaxRsClientTest
 {
-
+    /** 
+     * Request URLs of the RESTful web service
+     */
     private static final String XML_URL = "http://localhost:8080/jboss-as-helloworld-rs/xml";
     private static final String JSON_URL = "http://localhost:8080/jboss-as-helloworld-rs/json";
-
+    
     /**
-     *  Constructor initializing calls to runRequest method.
+     * Responses of the RESTful web service
      */
-    public JaxRsClient()
+    private static final String XML_RESPONSE = "<xml><result>Hello World!</result></xml>";
+    private static final String JSON_RESPONSE = "{\"result\":\"Hello World!\"}";
+    
+    
+    /**
+     * Test method which executes the runRequest method that calls the RESTful 
+     * helloworld-rs web service.
+     */
+    @Test
+    public void test()
     {
-        this.runRequest(JaxRsClient.XML_URL, MediaType.APPLICATION_XML_TYPE);
-        this.runRequest(JaxRsClient.JSON_URL, MediaType.APPLICATION_JSON_TYPE);
+        assertEquals("XML Response", JaxRsClientTest.XML_RESPONSE, this.runRequest(JaxRsClientTest.XML_URL, MediaType.APPLICATION_XML_TYPE));
+        assertEquals("JSON Response", JaxRsClientTest.JSON_RESPONSE, this.runRequest(JaxRsClientTest.JSON_URL, MediaType.APPLICATION_JSON_TYPE));
     }
-
+    
     /**
      * The purpose of this method is to run the external REST request.
      * @param url The url of the RESTful service
      * @param mediaType The mediatype of the RESTful service
      */
-    private void runRequest(String url, MediaType mediaType)
+    private String runRequest(String url, MediaType mediaType)
     {
+        String result = null;
+        
         System.out.println("===============================================");
         System.out.println("URL: " + url);
         System.out.println("MediaType: " + mediaType.toString());
@@ -70,14 +92,14 @@ public class JaxRsClient
                     new ByteArrayInputStream(response.getEntity().getBytes())));
 
             // Loop over the br in order to print out the contents
-            String output;
             System.out.println("\n*** Response from Server ***\n");
+            String output = null;
             while ((output = br.readLine()) != null)
             {
                 System.out.println(output);
+                result = output;
             }
         }
-
         catch (ClientProtocolException cpe)
         {
             System.err.println(cpe);
@@ -92,15 +114,8 @@ public class JaxRsClient
         }
         
         System.out.println("\n===============================================");
+        
+        return result;
     }
 
-    /**
-     * This is the main method of the class which kicks off the processing
-     *
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        new JaxRsClient();
-    }
 }
